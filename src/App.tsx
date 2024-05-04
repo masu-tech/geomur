71e5f05283abd49939521d7970e9da04970ca70f
misty fineal <misty@mistynoMac-Studio.local>
2024-05-04 23:26:45 +0900

    ナビゲーション実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Amplify } from 'aws-amplify'
 import awsconfig from './aws-exports'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AppNavigator from '@/navigations/AppNavigator';
 
-Amplify.configure(awsconfig)
+Amplify.configure(awsconfig);
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <AppNavigator />
     </View>
+
   );
 }
 
@@ -19,7 +24,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
---
+++ src/screens/LeaveScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LeaveScreen = () => {
+  return (
+    <View>
+      <Text>投稿画面</Text>
+    </View>
+  );
+};
+
+export default LeaveScreen;
